0e83eafff120a56f70b0fe9cf7b08fb91b3ec234
Edouard Dhalluin <ed.dhalluin@gmail.com>
2021-02-11 10:48:16 +0100

    Maj du resposive

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -18,54 +18,53 @@ const AddTodo = () => {
 
     const addTodoAction = () => {
         // Faire en sorte que le champ de saisie soit réinitialisé/vidé après l'ajout de la tâche
-        
-        let gift;
-        if(nice)
-            {
-             gift = datanice[Math.floor(Math.random()*datanice.length)]
-            }
-            else{
-             gift = databad[Math.floor(Math.random()*databad.length)]
 
-            }       
-        dispatch(addTodo(text,gift));
+        let gift;
+        if (nice) {
+            gift = datanice[Math.floor(Math.random() * datanice.length)]
+        }
+        else {
+            gift = databad[Math.floor(Math.random() * databad.length)]
+
+        }
+        dispatch(addTodo(text, gift));
         setText("");
     }
     return (
         <div className="form-group">
             <input
                 type="text"
-                className="todo-input"
+                className="todo-input-text"
                 onChange={(e) => setText(e.target.value)}
-                value={text} 
-                placeholder ="prénom de l'enfant "
+                value={text}
+                placeholder="prénom de l'enfant "
             />
             <div className="div-radio">
-            <input
-            type="radio"
-            id="nice"
-            name="niceornot"
-            value="nice"
-            onChange={(e) => setNice(e.target.checked) }
-            checked ={nice}
-            />
-            <label for ="nice"
-            className="todoGift">
-                Enfant  sage
+                <input
+                    type="radio"
+                    id="nice"
+                    name="niceornot"
+                    value="nice"
+                    onChange={(e) => setNice(e.target.checked)}
+                    checked={nice}
+                />
+                <label for="nice"
+                    className="todoGift">
+                    Enfant  sage
             </label>
             </div>
             <div className="div-radio">
-            <input
-            type="radio"
-            id="bad"
-            name="niceornot"
-            value="bad"
-            onChange={(e) => setNice(!e.target.checked) }
-            checked ={!nice}
-            />
-            <label for ="bad"
-            className="todoGift">
-                Enfant pas sage
+                <input
+                    type="radio"
+                    id="bad"
+                    name="niceornot"
+                    value="bad"
+                    onChange={(e) => setNice(!e.target.checked)}
+                    checked={!nice}
+                />
+                <label for="bad"
+                    className="todoGift">
+                    Enfant pas sage
             </label>
             </div>
             <Button
